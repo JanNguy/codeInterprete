6af14ec1328f9de4e7a5dae140016ba814576472
Jan Nguyen <jan.nguyen@epitech.eu>
2025-12-24 05:44:00 +0100

    fix: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::fs;
+use std::{collections::{HashMap}, fs, process};
 mod utils;
+mod parsers;
 
 fn main() {
-    let file: Vec<char> = fs::read_to_string("./file").unwrap().chars().collect();
+    let file: Vec<char> = fs::read_to_string("./test/python_shebang").unwrap().chars().collect();
+    let mut status: String = String::new();
+    let mut lang: HashMap<String, String> = HashMap::new();
 
-    match utils::get_shebang::get_shebang(file) {
-        Ok(shebang) => println!("{:?}", shebang),
-        Err(e) => println!("Error: {}", e),
+    lang = utils::insert_lang::insert_lang();
+    let sh = match utils::get_shebang::get_shebang(file) {
+        Ok(shebang) => {
+            println!("{:?}", shebang);
+            shebang
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+            process::exit(84);
+        },
+    };
+    status = parsers::req_p::req_p(&lang, sh);
+    if status.contains("Error: ") {
+        process::exit(84);
     }
+    println!("{}", status);
 }
